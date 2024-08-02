ca4ee941f98affccc5a6cef94369fe7a30c7641d
Smaugopp <163888260+Smaugopp@users.noreply.github.com>
2024-08-02 21:59:56 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -3,32 +3,34 @@ from pyrogram.types import InlineKeyboardButton
 import config
 from AviaxMusic import app
 
-def start_panel(localization):
+
+def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=localization["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=localization["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
     ]
     return buttons
 
-def private_panel(localization):
+
+def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=localization["S_B_3"],
+                text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=localization["S_B_4"], callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=localization["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
         [
-            InlineKeyboardButton(text=localization["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=localization["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
     ]
     return buttons
